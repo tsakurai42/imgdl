5a24b493d05b883459a26db867c4ddcf0a0036dc
tsakurai42 <tsakurai42@gmail.com>
2022-09-05 14:23:26 -0600

    all runs off of json and requests with no splinter/bs4

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # imgdl
-Image downloading script
+An image/video downloading script for fun
+
+Takes a newline delimited text file of JSONs from either twitter or tiktok, 
+finds the image or video URLs and downloads them and saves them in a file
+name format that I like.
+
+Knows to stop downloading tiktoks once it hits a video it has seen before.
+Also will not download if the video it's trying to download already exists.
--- imgdl.py
+++ imgdl.py
@@ -1,115 +1,126 @@
 from requests import get
-from bs4 import BeautifulSoup
+import os.path
 import re
-import time
-from splinter import Browser
+# from splinter import Browser
+# from bs4 import BeautifulSoup
+# import time
 import json
 import glob
 
-browser = Browser('firefox', **{'executable_path': 'C:/Users/rawr/Downloads/geckodriver.exe'})
+# browser = Browser('firefox', **{'executable_path': 'C:/Users/rawr/Downloads/geckodriver.exe'})
 
 url_file = open('urls.txt','a')
-
+url_file.truncate(0)
 files = [file for file in glob.glob('json/*')]
 for file_num, file_name in enumerate(files):
     f0 = open(file_name,encoding='utf-8')
-    f1 = json.load(f0)
-    g = f1['data']['bookmark_timeline']['timeline']['instructions'][0]['entries']
-    for _ in g:
-        if 'tweet' in _['entryId']:
-            try:
-                screenname = _['content']['itemContent']['tweet_results']['result']['core']['user_results']['result']['legacy']['screen_name']
-                tweet_id = _['content']['itemContent']['tweet_results']['result']['rest_id']
-                url_file.write(f'https://twitter.com/{screenname}/status/{tweet_id}\n')
-            except:
-                continue
+    for line in f0:
+        f1 = json.loads(line)
+        if 'data' in f1:
+            g = f1['data']['bookmark_timeline']['timeline']['instructions'][0]['entries']
+            for _ in g:
+                if 'tweet' in _['entryId']:
+                    try:
+                        if 'thumb' in _['content']['itemContent']['tweet_results']['result']['legacy']['entities']['media'][0]['media_url_https']:
+                            continue
+                        url_to_write = 'twitter#$%$#'
+                        expanded_url = _['content']['itemContent']['tweet_results']['result']['legacy']['entities']['media'][0][
+                            'expanded_url'][8:-8]
+                        url_to_write += f'{expanded_url}'
+                        for each_dict in _['content']['itemContent']['tweet_results']['result']['legacy']['entities']['media']:
+                            # print(each_dict)
+                            # print(each_dict['media_url_https'])
+                            url_split = each_dict['media_url_https'].rsplit('.',1)
+                            url = f'{url_split[0]}?format={url_split[1]}&name=orig'
+                            url_to_write += f'#$%$#{url}'
+
+                        # for _ in range(len(_['content']['itemContent']['tweet_results']['result']['legacy']['entities']['media'])):
+                        #     url_original = _['content']['itemContent']['tweet_results']['result']['legacy']['entities']['media'][_]['media_url_https']
+                        #     url_split = url_original.rsplit('.', 1)
+                        #     url = f'{url_split[0]}?format={url_split[1]}&name=orig'
+
+                        url_file.write(f'{url_to_write}\n')
+                    except:
+                        continue
+        elif 'itemList' in f1:      #tiktoks have an itemlist
+            for _ in f1['itemList']:
+                tiktok_id = _['id']
+                tiktok_desc = re.sub(r"[^A-Za-z0-9 #!]+","",_['desc'])
+                tiktok_videourl = _['video']['downloadAddr']
+                tiktok_author = _['author']['uniqueId']
+                try:
+                    url_file.write(f'tiktok#$%$#{tiktok_id}#$%$#{tiktok_videourl}#$%$#{tiktok_desc}#$%$#{tiktok_author}\n')
+                except:
+                    print(tiktok_desc)  # descriptions with emojis were throwing an error. regexed removed above now
+                    continue
 url_file.close()
 
 f = open('urls.txt', 'r')
 url_list = [l.strip('\n') for l in f.readlines()]
 f.close()
 total_dl = len(url_list)
-for count, url in enumerate(url_list):
-    print(f'Downloading #{count+1} of {total_dl}: {url}')
-    browser.visit(url)
-    time.sleep(2)
-    soup = BeautifulSoup(browser.html, 'html.parser')
-    domain = url.split('/')[2]
-    if 'instagram' in domain:
-        print(domain)
-        img_html = soup.find_all('img')
-        img_url = img_html[0].get('src')
-        print(img_url)
-        req = get(img_url)
-        file = open(f'images/{url.split("://")[1][4:].replace("/", "")}.jpg', 'wb')
-        for chunk in req.iter_content(1000000):
-            file.write(chunk)
-        file.close()
 
-    elif 'twitter' in domain:
-        try:
-            prim_div = soup.find_all('div', attrs={
-                'style': 'transform: translateY(0px); position: absolute; width: 100%; transition: opacity 0.3s ease-out 0s;'})[
-                0]
-        except:
-            print(f"Couldn't find the right div for images")
-            f = open('manual.txt', 'a')
-            f.write(f'{url[8:]}\n')
-            f.close()
+skiptok = 0
+first_tiktok = 0
+
+with open('lasttiktok.txt') as lasttok_file:
+    last_tiktok = lasttok_file.readline().strip().split("#$%$#")[0]
+
+for count, pre_url in enumerate(url_list):
+    url = pre_url.split('#$%$#')
+    if url[0] == 'twitter':
+        print(f'Downloading #{count + 1} of {total_dl}: {url[1]}',flush=True)
+        for each_image in range(2,len(url)):
+            # print(each_image)
+            req = get(url[each_image])
+            if req.status_code != 200:
+                for _ in range(3):
+                    req = get(url[each_image])
+                    if req.status_code == 200:
+                        break
+                print(req.status_code)
+                f2 = open('redo.txt', 'a')
+                f2.write(f'{url[1]}\n')
+                f2.close()
+            if len(url) == 3:
+                file = open(f'{url[1].replace("/","")}.jpg', 'wb')
+            else:
+                file = open(f'{url[1].replace("/", "")} {each_image-1}.jpg', 'wb')
+            for chunk in req.iter_content(1000000):
+                file.write(chunk)
+            file.close()
+    elif skiptok != 0:
+        print(f'Skipping this tiktok, already downloaded',flush=True)
+    elif url[0] == 'tiktok':
+        # print(url[1])
+        if first_tiktok == 0:
+            # print(f'1st if')
+            with open('lasttiktok.txt', 'r+') as lasttok_file:
+                lasttok_file.truncate(0)
+                lasttok_file.write(f'{url[1]}#$%$#{url[4]}')    # write first one to file
+            first_tiktok = 1
+        if url[1] == last_tiktok:
+            # print(f'2nd if')
+            skiptok = 1
+            print(f'All tiktoks from here on out were already downloaded',flush=True)
             continue
-        if prim_div.findAll(
-                text='Age-restricted adult content. This content might not be appropriate for people under 18 years old. To view this media, you’ll need to '):
-            print(f'Age-restricted, Written to manual.txt')
-            f = open('manual.txt', 'a')
-            f.write(f'{url[8:]}\n')
-            f.close()
-
+        print(f'Downloading #{count + 1} of {total_dl}: @{url[4]} - {url[1]}',flush=True)
+        if os.path.isfile(f'@{url[4]} - {url[1]} - {url[3][:60]}.mp4'):
+            print('already downloaded')
             continue
-
-        if prim_div.findAll(text='The following media includes potentially sensitive content. '):
-            # XPath of the view button
-            # xpath = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[1]/div/div/article/div/div/div/div[3]/div[2]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/span/span'
-            # xpath = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[1]/div/div/article/div/div/div/div[3]/div[2]/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/span/span'
-            xpath = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[3]/div[2]/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/span/span'
-            try:
-                browser.find_by_xpath(xpath).first.click()
-                time.sleep(1)
-                soup = BeautifulSoup(browser.html, 'html.parser')
-                prim_div = soup.find_all('div', attrs={
-                    'style': 'transform: translateY(0px); position: absolute; width: 100%; transition: opacity 0.3s ease-out 0s;'})[
-                    0]
-            except:
-                print(f"XPath may have changed? Written to redo.txt")
-                f = open('redo.txt', 'a')
-                f.write(f'{url[8:]}\n')
-                f.close()
-                continue
-
-        try:
-            img_url = prim_div.select_one('img[alt="Image"]').get('src')
-        except:
-            print("Couldn't find the image, maybe it has alt text")
-            f = open('manual.txt', 'a')
-            f.write(f'{url[8:]}\n')
-            f.close()
-            continue
-        split_url = re.split('[?&]', img_url)
-        img_url = f'{split_url[0]}?{split_url[1]}&name=orig'
-
-        req = get(img_url)
+        req = get(url[2])
         if req.status_code != 200:
             for _ in range(3):
-                req = get(img_url)
+                req = get(url[2])
                 if req.status_code == 200:
                     break
             print(req.status_code)
             f2 = open('redo.txt', 'a')
-            f2.write(f'{url}\n')
+            f2.write(f'tiktok.com/@{url[4]}/video/{url[1]}\n')
             f2.close()
-
-        file = open(f'{url.split("://")[1].replace("/", "")}.{split_url[1].split("=")[1]}', 'wb')
+        file = open(f'@{url[4]} - {url[1]} - {url[3][:60]}.mp4', 'wb')
         for chunk in req.iter_content(1000000):
             file.write(chunk)
         file.close()
-
-browser.quit()
+    else:
+        print('wtf did you doooo?')
